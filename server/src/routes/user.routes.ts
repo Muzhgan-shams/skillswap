7727565cfc928d9c074030b73d0d24f5b788ee1e
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-03-17 23:57:21 +0500

    feat(routes): add login route with validation to user routes

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller";
+import { loginUser, registerUser } from "../controllers/user.controller";
 import { validate } from "../middleware/validate";
-import { registerSchema } from "../validators/user.validator";
+import { loginSchema, registerSchema } from "../validators/user.validator";
 
 const router = Router();
 
 // POST /api/users/register
 router.post("/register", validate(registerSchema), registerUser);
 
+// POST /api/users/login
+router.post("/login", validate(loginSchema), loginUser);
 export default router;
