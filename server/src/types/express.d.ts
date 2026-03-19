6366e533ade0e0732b6cc50288df842ade32bd49
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-03-19 23:53:33 +0500

    chore: add comments to express extend type

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,7 +1,9 @@
 import "express";
 
 declare module "express" {
+  // module augmentation to add custom fields to the Express Request type
   interface Request {
+    // to add userId and userEmail to the Express Request type field
     userId?: number;
     userEmail?: string;
   }
