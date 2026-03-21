3c760c5b84a3cefa22248eb7ddfbdc45582bd0c5
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-03-21 23:01:28 +0500

    feat: add getMe route

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller";
+import { getMe, loginUser, registerUser } from "../controllers/user.controller";
 import { validate } from "../middleware/validate";
 import { loginSchema, registerSchema } from "../validators/user.validator";
+import { authenticate } from "../middleware/auth";
 
 const router = Router();
-
+/* Public Routes */
 // POST /api/users/register
 router.post("/register", validate(registerSchema), registerUser);
 
 // POST /api/users/login
 router.post("/login", validate(loginSchema), loginUser);
+
+/* Private Routes */
+router.get("/me", authenticate, getMe);
+
 export default router;
