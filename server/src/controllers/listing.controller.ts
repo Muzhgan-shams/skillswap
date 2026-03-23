099a22ba98d7ed8a2602aa5cab7828a0e70fb06f
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-03-23 23:58:35 +0500

    feat(listing): add create listing controller functionality

--- server/src/controllers/listing.controller.ts
+++ server/src/controllers/listing.controller.ts
@@ -1,6 +1,47 @@
 import type { Request, Response } from "express";
+import prisma from "../lib/prisma";
+import { CreateListingInput } from "../validators/listing.validator";
 
 export const createListing = async (
   req: Request,
   res: Response,
-): Promise<void> => {};
+): Promise<void> => {
+  const { title, description, category, skillOffered, skillWanted, hours } =
+    req.body as CreateListingInput;
+
+  const listing = await prisma.listing.create({
+    data: {
+      userId: req.userId as number,
+      title,
+      description,
+      category,
+      skillOffered,
+      skillWanted,
+      hours,
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      category: true,
+      skillOffered: true,
+      skillWanted: true,
+      hours: true,
+      isAvailable: true,
+      createdAt: true,
+      user: {
+        select: {
+          id: true,
+          username: true,
+          city: true,
+        },
+      },
+    },
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Listing created successfully",
+    data: listing,
+  });
+};
