e833d1cfe20a9e723423921950b06db75c7da95a
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-04-07 23:11:14 +0500

    feat(message): mount messageRoutes to index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,10 @@ import { env } from "./config/env";
 import express from "express";
 import type { Request, Response } from "express";
 import cors from "cors";
-import userRoutes from "./routes/user.routes";
-import listingRoutes from "./routes/listing.routes";
-import requestRoutes from "./routes/request.routes";
+import userRoutes from "./routes/user.routes.js";
+import listingRoutes from "./routes/listing.routes.js";
+import requestRoutes from "./routes/request.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 const app = express();
 const PORT = env.PORT;
@@ -20,6 +21,7 @@ app.get("/api/health", (_req: Request, res: Response) => {
 app.use("/api/users", userRoutes);
 app.use("/api/listings", listingRoutes);
 app.use("/api/requests", requestRoutes);
+app.use("/api/messages", messageRoutes);
 
 // Start the server
 app.listen(PORT, () => {
