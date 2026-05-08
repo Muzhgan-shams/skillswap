cd27d69e4476ec7a54d8e0400c75efd672ea4501
Muzhgan-shams <muzhgan_shams@yahoo.com>
2026-05-08 23:52:27 +0500

    feat(routes): implement routing with Routes and Route components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,42 @@
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-4xl">SkillSwap</h1>
+    <div className="min-h-screen bg-gray-50">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="p-8 text-2xl font-bold text-blue-600">
+              Home Page
+            </div>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <div className="p-8 text-2xl font-bold text-blue-600">
+              Login Page
+            </div>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <div className="p-8 text-2xl font-bold text-green-600">
+              Register Page
+            </div>
+          }
+        />
+        <Route
+          path="*" /* the wildcard route, matches anyy URL that did not match anything above */
+          element={
+            <div className="p-8 text-2xl font-bold text-red-600">
+              404 — Page Not Found
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 };
--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,9 +1,11 @@
+/* One import everywhere */
 import axios from "axios";
 //custom axios instance with base URL
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
+// telling axios app's specific bahavior
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
